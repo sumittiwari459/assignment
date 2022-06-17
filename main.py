00095ca2dd63628f194a766cb0cc7da8eaaf7ef8
sumittiwari459 <106306439+sumittiwari459@users.noreply.github.com>
2022-06-17 18:06:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,8 @@ def add_command():
 
 def delete_command():
     backend.delete(selected_tuple[0])
-
-# def update_command():
-#     backend.update(selected_tuple[0],food_name.get(),type.get(),cost.get())
+    
+# ****** if we want output in next window******
 
 # window2=Tk()
 
@@ -49,7 +48,7 @@ def delete_command():
 # list1.bind('<<ListboxSelect>>',get_selected_row)
 
 
-
+# Create frame
 window=Tk()
 window.geometry("520x300")
 window.wm_title("Restaurent")
@@ -68,6 +67,7 @@ label1 = Label(image=img)
 label1.image = img
 label1.grid(row=7,column=3)
 
+# creating label
 
 l1=Label(window,text="FOODNAME",fg = "RED")
 l1.grid(row=1,column=0,padx=5, pady=5)
@@ -77,6 +77,9 @@ l2.grid(row=1,column=2,padx=5, pady=5)
 
 l3=Label(window,text="COST",fg = "RED")
 l3.grid(row=2,column=0,padx=5, pady=5)
+
+
+# creating Textfield
 
 food_name=StringVar()
 e1=Entry(window,textvariable=food_name,bd=5)
@@ -90,9 +93,12 @@ cost=StringVar()
 e3=Entry(window,textvariable=cost,bd=5)
 e3.grid(row=2,column=1,padx=5, pady=5)
 
+#creating Listbox
 
 list1=Listbox(window, height=6,width=30,activestyle = 'dotbox', font = "Helvetica",fg = "RED")
 list1.grid(row=6,column=0,rowspan=7,columnspan=2)
+
+# Creating scrollbar 
 
 sb1=Scrollbar(window)
 sb1.grid(row=6,column=2,rowspan=6)
@@ -102,6 +108,8 @@ sb1.configure(command=list1.yview)
 
 list1.bind('<<ListboxSelect>>',get_selected_row)
 
+#Creating Buttons
+
 b1=Button(window,text="View all", width=12,bg='green',command=view_command)
 b1.grid(row=0,column=0,padx=5, pady=5)
 
@@ -110,9 +118,6 @@ b2.grid(row=0,column=1,padx=5, pady=5)
 
 b3=Button(window,text="Add entry", width=12,bg='YELLOW',command=add_command)
 b3.grid(row=0,column=2,padx=5, pady=5)
-
-# b4=Button(window,text="Update selected", width=12,command=update_command)
-# b4.grid(row=5,column=3)
 
 b5=Button(window,text="Delete selected", width=12,bg='PINK',command=delete_command)
 b5.grid(row=0,column=3,padx=5, pady=5)
